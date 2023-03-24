6be136f874ecbae4949c5540675a8a0f72c69677
StanislavChlup <116007540+StanislavChlup@users.noreply.github.com>
2023-03-24 21:05:10 +0100

    Update main.js
    I was completly wrong another mistake

--- main.js
+++ main.js
@@ -203,34 +203,34 @@ var firstp;
 var playfield;
 
 const unrevieled = new Image();
-unrevieled.src = './images/unrevieled.png';
+unrevieled.src = '.imgs/unrevieled.png';
 const flag = new Image();
-flag.src = './images/flag.png';
+flag.src = './imgs/flag.png';
 const exploded = new Image();
-exploded.src = './images/exploded.png';
+exploded.src = './imgs/exploded.png';
 const mine = new Image();
-mine.src = './images/mine.png';
+mine.src = './imgs/mine.png';
 const flagmissplaced = new Image();
-flagmissplaced.src = './images/flagmissplaced.png';
+flagmissplaced.src = './imgs/flagmissplaced.png';
 
 const _0 = new Image();
-_0.src = './images/p0.png';
+_0.src = './imgs/p0.png';
 var _1 = new Image();
-_1.src = './images/p1.png';
+_1.src = './imgs/p1.png';
 const _2 = new Image();
-_2.src = './images/p2.png';
+_2.src = './imgs/p2.png';
 const _3 = new Image();
-_3.src = './images/p3.png';
+_3.src = './imgs/p3.png';
 const _4 = new Image();
-_4.src = './images/p4.png';
+_4.src = './imgs/p4.png';
 const _5 = new Image();
-_5.src = './images/p5.png';
+_5.src = './imgs/p5.png';
 const _6 = new Image();
-_6.src = './images/p6.png';
+_6.src = './imgs/p6.png';
 const _7 = new Image();
-_7.src = './images/p7.png';
+_7.src = './imgs/p7.png';
 const _8 = new Image();
-_8.src = './images/p8.png';
+_8.src = './imgs/p8.png';
 
 
 
